ebe3956543e881188612a179394643bf38fb2e04
Davis Roldán <droldan-2019310@kinal.edu.gt>
2021-04-28 02:28:02 -0300

    SE SOLUCIONARON ERRORES EN FACTURACIÓN

--- query Cambios Davis.sql
+++ query Cambios Davis.sql
@@ -99,4 +99,18 @@ DELIMITER $$
 				pr.productoId ASC;
         END $$
 DELIMITER ;
-        
+
+
+-- CAMBIOS QUE SE HICIERON
+
+DROP PROCEDURE IF EXISTS SpListarBackup;
+
+DELIMITER $$
+	create procedure SpListarBackup(userName varchar(30))
+		BEGIN
+			select fdb.facturaDetalleIdBackup, p.productoId,p.productoDesc, fdb.cantidadBackup , p.productoPrecio ,fdb.totalParcialBackup
+				from facturadetallebackup as fdb
+							inner join Productos as p
+								on fdb.productoIdBackup = p.productoId;
+        END $$
+DELIMITER ;
