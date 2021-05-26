800e0c7a9c7cbcd305153bf230d254087391c5af
Davis Roldán <droldan-2019310@kinal.edu.gt>
2021-05-26 16:32:36 -0300

    se editó el proceso de sumar inventario

--- query Cambios Davis.sql
+++ query Cambios Davis.sql
@@ -1,12 +1,13 @@
 -- procreso de actualizar inventario desde factura - 8/04/2021
 DELIMITER $$
-	create procedure SpActualizarInventarioProductosFacturacion(idBuscado varchar(7), cant int(100),estado tinyint(1))
+	create procedure SpActualizarInventarioProductosFacturacion(idBuscado varchar(7), cant double, estado tinyint(1))
 		BEGIN
 			update InventarioProductos
 				set  estadoProductoId = estado, inventarioproductos.inventarioProductoCant = cant
 					where productoId = idBuscado;
         END $$
 DELIMITER ;
+
 
 
 DELIMITER $$ 
